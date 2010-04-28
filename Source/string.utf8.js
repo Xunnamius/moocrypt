ce638e7013d4bf88784394335a82092f829d957a
sixtyseconds <chris@sixtyseconds.co.za>
2010-04-28 11:07:30 +0200

    Fixed whitespace nonsense.

--- Source/string.utf8.js
+++ Source/string.utf8.js
@@ -18,30 +18,30 @@ provides: [String.tuUTF8]
 String.implement({
     'toUTF8': function()
     {
-		var utftext = '';
-		string = this.replace(/\r\n/g,"\n");
- 
-		for (var n = 0, l = string.length; n < l; n++)
+        var utftext = '';
+        string = this.replace(/\r\n/g,"\n");
+        
+        for (var n = 0, l = string.length; n < l; n++)
         {
-			var c = string.charCodeAt(n);
- 
-			if (c < 128)
+            var c = string.charCodeAt(n);
+            
+            if (c < 128)
             {
-				utftext += String.fromCharCode(c);
-			}
-			else if ((c > 127) && (c < 2048))
+                utftext += String.fromCharCode(c);
+            }
+            else if ((c > 127) && (c < 2048))
             {
-				utftext += String.fromCharCode((c >> 6) | 192);
-				utftext += String.fromCharCode((c & 63) | 128);
-			}
-			else
+                utftext += String.fromCharCode((c >> 6) | 192);
+                utftext += String.fromCharCode((c & 63) | 128);
+            }
+            else
             {
-				utftext += String.fromCharCode((c >> 12) | 224);
-				utftext += String.fromCharCode(((c >> 6) & 63) | 128);
-				utftext += String.fromCharCode((c & 63) | 128);
-			}
-		}
- 
-		return utftext;
+                utftext += String.fromCharCode((c >> 12) | 224);
+                utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+                utftext += String.fromCharCode((c & 63) | 128);
+            }
+        }
+        
+        return utftext;
     }
 });
