b392892c00205a0f336a50dc96516b99fe085d5b
sixtyseconds <chris@sixtyseconds.co.za>
2010-04-28 14:33:47 +0200

    Neatened.

--- Source/string.utf8.js
+++ Source/string.utf8.js
@@ -17,36 +17,37 @@ provides: [String.toUTF8]
 
 (function() {
     
-    function from(c)
+    function utf8(string)
     {
-        return String.fromCharCode(c);
+        var a, b, result = '',
+            from = String.fromCharCode;
+            
+        for (a = 0; b = string.charCodeAt(a); a++)
+        {
+            if (b < 128)
+            {
+                result += from(b);
+            }
+            else if ((b > 127) && (b < 2048))
+            {
+                result += from((b >> 6) | 192);
+                result += from((b & 63) | 128);
+            }
+            else
+            {
+                result += from((b >> 12) | 224);
+                result += from(((b >> 6) & 63) | 128);
+                result += from((b & 63) | 128);
+            }
+        }
+        
+        return result;
     }
 
     String.implement({
         'toUTF8': function()
         {
-            var a, b, c = '';
-            
-            for (a = 0; b = this.charCodeAt(a); a++)
-            {
-                if (b < 128)
-                {
-                    c += from(b);
-                }
-                else if ((b > 127) && (b < 2048))
-                {
-                    c += from((b >> 6) | 192);
-                    c += from((b & 63) | 128);
-                }
-                else
-                {
-                    c += from((b >> 12) | 224);
-                    c += from(((b >> 6) & 63) | 128);
-                    c += from((b & 63) | 128);
-                }
-            }
-            
-            return c;
+            return utf8(this);
         }
     });
 
