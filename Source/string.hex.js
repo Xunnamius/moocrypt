ce26b1f7842b882d025add0ad963b6d869b51cc0
Christopher Pitt <chris@sixtyseconds.co.za>
2010-04-29 11:38:45 +0200

    Whitespace formatting.

--- Source/string.hex.js
+++ Source/string.hex.js
@@ -16,33 +16,33 @@ provides: [String.toHex, String.fromHex]
 */
 
 (function() {
-    
-    String.implement({
-        'toHex': function()
-        {
-            var r = '', i = 0,
-                limit = this.length;
-            
-            while(i < limit){
-                r += this.charCodeAt(i++).toString(16);
-            }
-            
-            return r;
-        },
-        'fromHex': function()
-        {
-            var r = s = '',
-                e = this.length;
-                
-            while(e >= 0)
-            {
-                s = e - 2;
-                r = String.fromCharCode('0x' + this.substring(s, e)) + r;
-                e = s;
-            }
-            
-            return r;
-        }
-    });
+
+	String.implement({
+		'toHex': function()
+		{
+			var r = '', i = 0,
+			limit = this.length;
+
+			while(i < limit){
+				r += this.charCodeAt(i++).toString(16);
+			}
+
+			return r;
+		},
+		'fromHex': function()
+		{
+			var r = s = '',
+			e = this.length;
+
+			while(e >= 0)
+			{
+				s = e - 2;
+				r = String.fromCharCode('0x' + this.substring(s, e)) + r;
+				e = s;
+			}
+
+			return r;
+		}
+	});
 
 })();
--- Source/string.utf8.js
+++ Source/string.utf8.js
@@ -16,39 +16,39 @@ provides: [String.toUTF8]
 */
 
 (function() {
-    
-    function utf8(string)
-    {
-        var a, b, result = '',
-            from = String.fromCharCode;
-            
-        for (a = 0; b = string.charCodeAt(a); a++)
-        {
-            if (b < 128)
-            {
-                result += from(b);
-            }
-            else if ((b > 127) && (b < 2048))
-            {
-                result += from((b >> 6) | 192);
-                result += from((b & 63) | 128);
-            }
-            else
-            {
-                result += from((b >> 12) | 224);
-                result += from(((b >> 6) & 63) | 128);
-                result += from((b & 63) | 128);
-            }
-        }
-        
-        return result;
-    }
 
-    String.implement({
-        'toUTF8': function()
-        {
-            return utf8(this);
-        }
-    });
+	function utf8(string)
+	{
+		var a, b, result = '',
+		from = String.fromCharCode;
+
+		for (a = 0; b = string.charCodeAt(a); a++)
+		{
+			if (b < 128)
+			{
+				result += from(b);
+			}
+			else if ((b > 127) && (b < 2048))
+			{
+				result += from((b >> 6) | 192);
+				result += from((b & 63) | 128);
+			}
+			else
+			{
+				result += from((b >> 12) | 224);
+				result += from(((b >> 6) & 63) | 128);
+				result += from((b & 63) | 128);
+			}
+		}
+
+		return result;
+	}
+
+	String.implement({
+		'toUTF8': function()
+		{
+			return utf8(this);
+		}
+	});
 
 })();
