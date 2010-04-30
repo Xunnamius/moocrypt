e9a427d2b17db023fae5c3ad661f2f4cd4b127a0
Christopher Pitt <chris@sixtyseconds.co.za>
2010-04-30 09:05:19 +0200

    Heavy formatting and optimization.

--- Source/string.sha1.js
+++ Source/string.sha1.js
@@ -17,140 +17,138 @@ provides: [String.toSHA1]
 */
 
 (function() {
-    
-    var transforms = {
-        'rotateLeft': function(a, b)
-        {
-    		return (a << b) | (a >>> (32 - b));
-        },
-        'hex': function(a)
-        {
-            var b, c, result = '';
-     
-    		for(b = 7; b >= 0; b--)
-            {
-    			c = (a >>> (b * 4)) & 0x0f;
-    			result += c.toString(16);
-    		}
-            
-    		return result;
-        }
-    };
-    
-    function sha1(msg)
-    {
-    	var blockstart;
-    	var i, j;
-    	var W = new Array(80);
-    	var H0 = 0x67452301;
-    	var H1 = 0xEFCDAB89;
-    	var H2 = 0x98BADCFE;
-    	var H3 = 0x10325476;
-    	var H4 = 0xC3D2E1F0;
-    	var A, B, C, D, E;
-    	var temp;
-     
-    	msg = msg.toUTF8();
-     
-    	var msg_len = msg.length;
-     
-    	var word_array = new Array();
-    	for( i=0; i<msg_len-3; i+=4 ) {
-    		j = msg.charCodeAt(i)<<24 | msg.charCodeAt(i+1)<<16 |
-    		msg.charCodeAt(i+2)<<8 | msg.charCodeAt(i+3);
-    		word_array.push( j );
-    	}
-     
-    	switch( msg_len % 4 ) {
-    		case 0:
-    			i = 0x080000000;
-    		break;
-    		case 1:
-    			i = msg.charCodeAt(msg_len-1)<<24 | 0x0800000;
-    		break;
-     
-    		case 2:
-    			i = msg.charCodeAt(msg_len-2)<<24 | msg.charCodeAt(msg_len-1)<<16 | 0x08000;
-    		break;
-     
-    		case 3:
-    			i = msg.charCodeAt(msg_len-3)<<24 | msg.charCodeAt(msg_len-2)<<16 | msg.charCodeAt(msg_len-1)<<8	| 0x80;
-    		break;
-    	}
-     
-    	word_array.push( i );
-     
-    	while( (word_array.length % 16) != 14 ) word_array.push( 0 );
-     
-    	word_array.push( msg_len>>>29 );
-    	word_array.push( (msg_len<<3)&0x0ffffffff );
-     
-     
-    	for ( blockstart=0; blockstart<word_array.length; blockstart+=16 ) {
-     
-    		for( i=0; i<16; i++ ) W[i] = word_array[blockstart+i];
-    		for( i=16; i<=79; i++ ) W[i] = transforms.rotateLeft(W[i-3] ^ W[i-8] ^ W[i-14] ^ W[i-16], 1);
-     
-    		A = H0;
-    		B = H1;
-    		C = H2;
-    		D = H3;
-    		E = H4;
-     
-    		for( i= 0; i<=19; i++ ) {
-    			temp = (transforms.rotateLeft(A,5) + ((B&C) | (~B&D)) + E + W[i] + 0x5A827999) & 0x0ffffffff;
-    			E = D;
-    			D = C;
-    			C = transforms.rotateLeft(B,30);
-    			B = A;
-    			A = temp;
-    		}
-     
-    		for( i=20; i<=39; i++ ) {
-    			temp = (transforms.rotateLeft(A,5) + (B ^ C ^ D) + E + W[i] + 0x6ED9EBA1) & 0x0ffffffff;
-    			E = D;
-    			D = C;
-    			C = transforms.rotateLeft(B,30);
-    			B = A;
-    			A = temp;
-    		}
-     
-    		for( i=40; i<=59; i++ ) {
-    			temp = (transforms.rotateLeft(A,5) + ((B&C) | (B&D) | (C&D)) + E + W[i] + 0x8F1BBCDC) & 0x0ffffffff;
-    			E = D;
-    			D = C;
-    			C = transforms.rotateLeft(B,30);
-    			B = A;
-    			A = temp;
-    		}
-     
-    		for( i=60; i<=79; i++ ) {
-    			temp = (transforms.rotateLeft(A,5) + (B ^ C ^ D) + E + W[i] + 0xCA62C1D6) & 0x0ffffffff;
-    			E = D;
-    			D = C;
-    			C = transforms.rotateLeft(B,30);
-    			B = A;
-    			A = temp;
-    		}
-     
-    		H0 = (H0 + A) & 0x0ffffffff;
-    		H1 = (H1 + B) & 0x0ffffffff;
-    		H2 = (H2 + C) & 0x0ffffffff;
-    		H3 = (H3 + D) & 0x0ffffffff;
-    		H4 = (H4 + E) & 0x0ffffffff;
-     
-    	}
-     
-    	var temp = transforms.hex(H0) + transforms.hex(H1) + transforms.hex(H2) + transforms.hex(H3) + transforms.hex(H4);
-     
-    	return temp.toLowerCase();
-     
-    }
-    
-    String.implement({
-       'toSHA1': function()
-       {
-           return sha1(this);
-       } 
-    });
+
+	var transforms = {
+		'rotateLeft': function(a, b)
+		{
+			return (a << b) | (a >>> (32 - b));
+		},
+		'hex': function(a)
+		{
+			var b, c, result = '';
+
+			for(b = 7; b >= 0; b--)
+			{
+				c = (a >>> (b * 4)) & 0x0f;
+				result += c.toString(16);
+			}
+
+			return result;
+		}
+	};
+
+	function sha1(string)
+	{
+		var a, b, c,
+			h1 = 0x67452301,
+			h2 = 0xEFCDAB89,
+			h3 = 0x98BADCFE,
+			h4 = 0x10325476,
+			h5 = 0xC3D2E1F0,
+			t1, t2, t3, t4, t5,
+
+			string = string.toUTF8(),
+			length = string.length,
+			words = new Array(),
+			buffer = new Array(80),
+
+			code = function(a) {
+				return string.charCodeAt(a);
+			},
+
+			assign = function(c) {
+				t5 = t4;
+				t4 = t3;
+				t3 = transforms.rotateLeft(t2, 30);
+				t2 = t1;
+				t1 = c
+			};
+
+		for(a = 0; a < length - 3; a += 4)
+		{
+			b = code(a) << 24 | code(a + 1) << 16 | code(a + 2) << 8 | code(a + 3);
+			words.push(b);
+		}
+
+		switch (length % 4)
+		{
+			case 0:
+				a = 0x080000000;
+				break;
+			case 1:
+				a = code(length - 1) << 24 | 0x0800000;
+				break;
+			case 2:
+				a = code(length - 2) << 24 | code(length - 1) << 16 | 0x08000;
+				break;
+			case 3:
+				a = code(length - 3) << 24 | code(length - 2) << 16 | code(length - 1) << 8 | 0x80;
+				break;
+		}
+
+		words.push(a);
+
+		while ((words.length % 16) != 14)
+		{
+			words.push(0);
+		}
+
+		words.push(length >>> 29);
+		words.push((length << 3) & 0x0ffffffff);
+
+		for (c = 0; c < words.length; c += 16)
+		{
+			for(a = 0; a < 16; a++)
+			{
+				buffer[a] = words[c + a];
+			}
+
+			for(a = 16; a <= 79; a++)
+			{
+				buffer[a] = transforms.rotateLeft(buffer[a - 3] ^ buffer[a - 8] ^ buffer[a - 14] ^ buffer[a - 16], 1);
+			}
+
+			t1 = h1;
+			t2 = h2;
+			t3 = h3;
+			t4 = h4;
+			t5 = h5;
+
+			for(a = 0; a <= 19; a++)
+			{
+				assign((transforms.rotateLeft(t1, 5) + ((t2 & t3) | (~t2 & t4)) + t5 + buffer[a] + 0x5A827999) & 0x0ffffffff);
+			}
+
+			for(a = 20; a <= 39; a++)
+			{
+				assign((transforms.rotateLeft(t1, 5) + (t2 ^ t3 ^ t4) + t5 + buffer[a] + 0x6ED9EBA1) & 0x0ffffffff);
+			}
+
+			for(a = 40; a <= 59; a++)
+			{
+				assign((transforms.rotateLeft(t1, 5) + ((t2 & t3) | (t2 & t4) | (t3 & t4)) + t5 + buffer[a] + 0x8F1BBCDC) & 0x0ffffffff);
+			}
+
+			for(a = 60; a <= 79; a++)
+			{
+				assign((transforms.rotateLeft(t1, 5) + (t2 ^ t3 ^ t4) + t5 + buffer[a] + 0xCA62C1D6) & 0x0ffffffff);
+			}
+
+			h1 = (h1 + t1) & 0x0ffffffff;
+			h2 = (h2 + t2) & 0x0ffffffff;
+			h3 = (h3 + t3) & 0x0ffffffff;
+			h4 = (h4 + t4) & 0x0ffffffff;
+			h5 = (h5 + t5) & 0x0ffffffff;
+		}
+
+		return (transforms.hex(h1) + transforms.hex(h2) + transforms.hex(h3) + transforms.hex(h4) + transforms.hex(h5)).toLowerCase();
+	}
+
+	String.implement({
+		'toSHA1': function()
+		{
+			return sha1(this);
+		}
+	});
+
 })();
