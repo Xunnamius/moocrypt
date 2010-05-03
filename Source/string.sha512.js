8f37f16582b3d11c1c6ce2fc29b04efe43a2df21
Christopher Pitt <chris@sixtyseconds.co.za>
2010-05-03 13:53:26 +0200

    Vastly simplified SHA512 implementation.

--- Source/string.sha512.js
+++ Source/string.sha512.js
@@ -11,146 +11,7 @@ requires:
 */
 
 (function() {
-
-	var transforms = {
-		'normal':
-		{
-			'hex': function(string, hexUpperCase)
-			{
-				return stringToHex(transforms.normal.raw(string.toUTF8()), hexUpperCase);
-			},
-			'base64': function(string, base64Pad)
-			{
-				return stringToBase64(transforms.normal.raw(string.toUTF8()), base64Pad);
-			},
-			'any': function(string, length)
-			{
-				return stringToAny(transforms.normal.raw(s.toUTF8()), length);
-			},
-			'raw': function(string)
-			{
-				return binToString(stringToBin(string).toSHA512(string.length * 8));
-			}
-		},
-		'hmac':
-		{
-			'hex': function(key, string, hexUpperCase)
-			{
-				return stringToHex(transforms.hmac.raw(key.toUTF8(), string.toUTF8()), hexUpperCase);
-			},
-			'base64': function(key, string, base64Pad)
-			{
-				return stringToBase64(transforms.hmac.raw(key.toUTF8(), string.toUTF8()), base64Pad);
-			},
-			'any': function(key, string, length)
-			{
-				return stringToAny(transforms.hmac.raw(key.toUTF8(), string.toUTF8()), length);
-			},
-			'raw': function(key, data)
-			{
-				var ipad = Array(32),
-					opad = Array(32),
-					bkey = stringToBin(key);
-
-				if(bkey.length > 32)
-				{
-					bkey = bkey.toSHA512(key.length * 8);
-				}
-
-				for(var i = 0; i < 32; i++)
-				{
-					ipad[i] = bkey[i] ^ 0x36363636;
-					opad[i] = bkey[i] ^ 0x5C5C5C5C;
-				}
-
-				var hash = ipad.concat(stringToBin(data)).toSHA512(1024 + data.length * 8);
-				return binToString(opad.concat(hash).toSHA512(1024 + 512));
-			}
-		}
-	};
-
-	function stringToHex(string, hexUpperCase)
-	{
-		var a, b, result = '',
-			characters = hexUpperCase ? "0123456789ABCDEF" : "0123456789abcdef";
-
-		for (a = 0; b = string.charCodeAt(a); a++)
-		{
-			result += characters.charAt((b >>> 4) & 0x0F) + characters.charAt(b & 0x0F);
-		}
-
-		return result;
-	}
-
-	function stringToBase64(string, base64Pad)
-	{
-		var a, b, c, d, result = '', length = string.length,
-			characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-
-		for (a = 0; b = string.charCodeAt(a); a += 3)
-		{
-			c = (b << 16) | (a + 1 < length ? string.charCodeAt(a + 1) << 8 : 0) | (a + 2 < length ? string.charCodeAt(a + 2) : 0);
-			
-			for (d = 0; d < 4; d++)
-			{
-				if (a * 8 + d * 6 > string.length * 8)
-				{
-					result += (base64Pad || '');
-					continue;
-				}
-				
-				result += characters.charAt((c >>> 6 * (3 - d)) & 0x3F);
-			}
-		}
-
-		return result;
-	}
-
-	function stringToAny(string, encoding)
-	{
-		var a, b, c, d, quotient, result = '',
-		
-			dividend = Array(Math.ceil(string.length / 2)),
-			divisor = encoding.length,
-			
-			length = Math.ceil(string.length * 8 / (Math.log(encoding.length) / Math.log(2))),
-			remainders = Array(length);
-
-		for (a = 0; a < dividend.length; a++)
-		{
-			dividend[a] = (string.charCodeAt(a * 2) << 8) | string.charCodeAt(a * 2 + 1);
-		}
-
-		for (a = 0; a < length; a++)
-		{
-			quotient = Array();
-			c = 0;
-
-			for(b = 0; b < dividend.length; b++)
-			{
-				c = (c << 16) + dividend[b];
-				d = Math.floor(c / divisor);
-				c -= d * divisor;
-
-				if (quotient.length > 0 || d > 0)
-				{
-					quotient[quotient.length] = d;
-				}
-			}
-
-			remainders[a] = x;
-			dividend = quotient;
-		}
-
-		var output = "";
-		for(i = remainders.length - 1; i >= 0; i--)
-		{
-			output += encoding.charAt(remainders[i]);
-		}
-
-		return output;
-	}
-
+	
 	function stringToBin(string)
 	{
 		var a, b = string.length,
@@ -169,49 +30,31 @@ requires:
 		return result;
 	}
 
-	function binToString(bin)
+	function binToHex(bin, hexUpperCase)
 	{
-		var a, result = '';
+		var result = '',
+			chars = hexUpperCase ? "0123456789ABCDEF" : "0123456789abcdef";
 
-		for(a = 0; a < bin.length * 32; a += 8)
+		for(var i = 0; i < bin.length * 4; i++)
 		{
-			result += String.fromCharCode((bin[a >> 5] >>> (24 - a % 32)) & 0xFF);
+			result += chars.charAt((bin[i >> 2] >> ((3 - i % 4) * 8 + 4)) & 0xF) + chars.charAt((bin[i >> 2] >> ((3 - i % 4) * 8  )) & 0xF);
 		}
 
 		return result;
 	}
+	
+	function sha512(string, size, hexUpperCase)
+	{
+		var bin = stringToBin(string.toUTF8()),
+			proc = bin.toSHA512(string.length * size);
+
+		return binToHex(proc, hexUpperCase);
+	}
 
 	String.implement({
-		'toSHA512': function(type, param)
+		'toSHA512': function()
 		{
-			var type = (type && type.toLowerCase()) || 'hex';
-
-			switch (type.toLowerCase())
-			{
-				case 'base64':
-					return transforms.normal.base64(this, param /* base64Pad */);
-					break;
-
-				case 'base64hmac':
-					return transforms.hmac.base64(this, param /* base64Pad */);
-					break;
-
-				case 'any':
-					return transforms.normal.any(this, param /* length */);
-					break;
-
-				case 'anyhmac':
-					return transforms.hmac.any(this, param /* length */);
-					break;
-
-				case 'hex':
-					return transforms.normal.hex(this, param /* hexUpperCase */);
-					break;
-
-				case 'hexhmac':
-					return transforms.hmac.hex(this, param /* hexUpperCase */);
-					break;
-			}
+			return sha512(this, 8, 0);
 		}
 	});
 
